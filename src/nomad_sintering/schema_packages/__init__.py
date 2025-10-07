1bcbd41e301508c3fa2cf5e7baa428322d76616b
Svetlana Grinman <svetlana.grinman@helmholtz-berlin.de>
2025-10-07 14:44:55 +0200

    Added sintering classes from yaml schema

--- src/nomad_sintering/schema_packages/__init__.py
+++ src/nomad_sintering/schema_packages/__init__.py
@@ -6,7 +6,7 @@ class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from sintering.schema_packages.schema_package import m_package
+        from nomad_sintering.schema_packages.schema_package import m_package
 
         return m_package
 
@@ -14,4 +14,17 @@ class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
 schema_package_entry_point = NewSchemaPackageEntryPoint(
     name='NewSchemaPackage',
     description='New schema package entry point configuration.',
+)
+
+class SinteringEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from nomad_sintering.schema_packages.sintering import m_package
+
+        return m_package
+
+
+sintering = SinteringEntryPoint(
+    name='Sintering',
+    description='Schema package for describing a sintering process.',
 )
